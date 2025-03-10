0c0e37829210fe7419c17dfe11c0a57eec9d3169
xmhu2001 <2268104085@qq.com>
2025-03-10 21:51:44 +0800

    Heap Sort

--- mySort.h
+++ mySort.h
@@ -40,7 +40,7 @@ void selectionSort(std::vector<int>& arr) {
     }
 }
 
-// 快排
+// 快速排序
 int partition(std::vector<int>&arr, int low, int high) {
     int pivot = arr[low];
     while(low<high) {
@@ -66,6 +66,54 @@ void quickSort(std::vector<int>& arr, int low, int high) {
         quickSort(arr, pi+1, high);
     }
 } 
+
+// 归并排序
+void mergeArray(std::vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    std::vector<int> L(n1), R(n2);
+    for(int i=0;i<n1;i++) L[i] = arr[left+i];
+    for(int i=0;i<n2;i++) R[i] = arr[mid+1+i];
+    int i=0, j=0, k=left;
+    while(i<n1 && j<n2) {
+        if(L[i]<=R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while(i<n1) arr[k++] = L[i++];
+    while(j<n2) arr[k++] = R[j++];
+}
+
+void mergeSort(std::vector<int>& arr, int left, int right) {
+    if(left<right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        mergeArray(arr, left, mid, right);   
+    }
+}
+
+// 堆排序
+// 通过数组存储，从最后一个父节点开始遍历，调整堆，之后调整子树
+void maxheapify(std::vector<int>& arr, int i, int n) {
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+    int largest = i;
+    if(left<n && arr[left]>arr[largest]) largest = left;
+    if(right<n && arr[right]>arr[largest]) largest = right;
+    if(largest!=i) {
+        std::swap(arr[i], arr[largest]);
+        maxheapify(arr, largest, n);
+    }
+}
+
+void heapSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for(int i=n/2-1;i>=0;i--) maxheapify(arr, i, n);
+    for(int i=n-1;i>=0;i--) {
+        std::swap(arr[0], arr[i]);
+        maxheapify(arr, 0, i);
+    }
+}
 
 }
 #endif
--- test.cpp
+++ test.cpp
@@ -9,12 +9,12 @@
 using namespace std;
 
 void testSortingAlgorithm(const string& name, void (*sortFunc)(vector<int>&)) {
-    // std::random_device rd;
-    // std::mt19937 gen(rd());
-    // std::uniform_int_distribution<int> dis(1, 100);
-    // std::vector<int> arr(100);
-    // for(int i=0;i<100;i++) arr[i] = dis(gen);
-    std::vector<int> arr = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(1, 100);
+    std::vector<int> arr(100);
+    for(int i=0;i<100;i++) arr[i] = dis(gen);
+    // std::vector<int> arr = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
     cout << "original arr: ";
     for(int num:arr) cout << num << " ";
     cout << endl;
@@ -43,8 +43,14 @@ int main() {
     auto quickSortTest = [](vector<int>& arr) {
         mysort::quickSort(arr, 0, arr.size()-1);
     };
-
     testSortingAlgorithm("Quick Sort", quickSortTest);
+
+    auto mergeSortTest = [](vector<int>& arr) {
+        mysort::mergeSort(arr, 0, arr.size()-1);
+    };
+    testSortingAlgorithm("Merge Sort", mergeSortTest);
+
+    testSortingAlgorithm("Heap Sort", mysort::heapSort);
 
     return 0;
 }
